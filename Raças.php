4e83618f6ab4dd08fd4111fa843ab7087c0c20f8
Usidhis <75745259+Usidhis@users.noreply.github.com>
2022-05-28 16:03:45 +0100

    Add files via upload
    Estilo quase terminado

--- Raças.php
+++ Raças.php
@@ -8,6 +8,19 @@ session_start();
     <title>DungeonPT - Raças</title>
     <link rel="stylesheet" href="CSS/raças.css">
 </head>
+
+
+<style>
+    table,
+    th,
+    td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        padding: 6px;
+    }
+
+</style>
+
 <body>
 
 <header>
@@ -84,90 +97,117 @@ session_start();
             </ul>
         </nav>
     </div>
+<table cellspacing="0" cellpadding="0" border="none">
+    <tr>
+        <td class="imagens">
 
-<div>
-<h3>Humano</h3>
-    Os humanos umas das raças comuns deste universo, com uma vida curta comparados a outras raças existentes. Por isso fazem o melhor com o pouco tempo que têm.
-    Por isso são uma raça inovadora e pioneira, tendo vástos impérios espalhados pelo mundo que duram eras e alguns nunca chegaram a ruir.
-    <p>
-        São uma raça que no geral mantem boas relações com as outras raças do mundo o que os ajuda a fazer poderosos aliados e a estabelcer comercio por todos os cantos do mundo.
-    </p>
-    <p>
-        Muitos humanos quando se tornam aventureiros fazem no por desejarem riquezas e fama ou para deixarem um legado grandioso, cuja história nunca será capaz de esquecer.
-    </p>
-    <h4>Traços da Raça:</h4>
-    <ul>
-        <li>São capazes de falar várias linguas por isso um personagem humano sabe Comum e outra lingua à sua escolha</li>
-        <li>São bastante equilibrados por isso um personagem ganha +1 em todos os atributos </li>
-    </ul>
-</div>
+                <img src="imagens/elf-removebg-preview.png">
 
-<div>
-    <h3>Elfos</h3>
-    Uma raça magica ligada à natureza. Vivem em florestas antigas ou em torres prateadas que duram milénios, por isso são muito ligados à arte, à música e à poesia.
-    Por viverem durante muitos anos os elfos dão valor às coisas simples do mundo.
-    <p>
-        São admirados por várias raças pela sua beleza incomparavel, as suas roupas enfatizam a sua beleza, no entanto estas são mais simples em comparação às das outras raças.
-        Um elfo pode ser dificil de lidar uma vez que são uma raça que se distância das outras, tento um orgulho que os torna perigosos, não se deve insultar um elfo sem esperar uma resposta deste.
-        Devido a estas caracteristicas não é facil criarem amizades com outras pessoas no entanto esta caracteristica também lhes premite criar inimigos.
-    </p>
-    <p>
-        Um elfo é uma mais valia para qualquer grupo de aventureiros, pois as suas habilidades marciais são conhecidas por todo o mundo, se tiveres um elfo a teu lado este não te vai deixar mal.
-    </p>
-    <h4>Traços da Raça:</h4>
-    <ul>
-        <li>Habituados a viver nas florestas e a crepusculos o personagem desta raça tem darkvision.</li>
-        <li>Argeis e rápidos, personagens desta raça ganha +2 na Dexterity </li>
-    </ul>
-    <h4>Sub-raças</h4>
-    Alto Elfo
-    <ul>
-        <li>O valor de Wisdom do personagem aumenta em 1.</li>
-        <li>O máximo de pontos de vida do personagem aumenta em 1, e cada vez que sobe de nível, recebe 1 ponto de vida adicional</li>
-    </ul>
+        </td>
+        <td colspan="2" class="texto">
+            <h3>Elfos</h3>
+            Uma raça magica ligada à natureza. Vivem em florestas antigas ou em torres prateadas que duram milénios, por isso são muito ligados à arte, à música e à poesia.
+            Por viverem durante muitos anos os elfos dão valor às coisas simples do mundo.
+            <p>
+                São admirados por várias raças pela sua beleza incomparavel, as suas roupas enfatizam a sua beleza, no entanto estas são mais simples em comparação às das outras raças.
+                Um elfo pode ser dificil de lidar uma vez que são uma raça que se distância das outras, tento um orgulho que os torna perigosos, não se deve insultar um elfo sem esperar uma resposta deste.
+                Devido a estas caracteristicas não é facil criarem amizades com outras pessoas no entanto esta caracteristica também lhes premite criar inimigos.
+            </p>
+            <p>
+                Um elfo é uma mais valia para qualquer grupo de aventureiros, pois as suas habilidades marciais são conhecidas por todo o mundo, se tiveres um elfo a teu lado este não te vai deixar mal.
+            </p>
+            <h4>Traços da Raça:</h4>
+            <ul>
+                <li>Habituados a viver nas florestas e a crepusculos o personagem desta raça tem darkvision.</li>
+                <li>Argeis e rápidos, personagens desta raça ganha +2 na Dexterity </li>
+            </ul>
+            <h4>Sub-raças</h4>
+            Alto Elfo
+            <ul>
+                <li>O valor de Wisdom do personagem aumenta em 1.</li>
+                <li>O máximo de pontos de vida do personagem aumenta em 1, e cada vez que sobe de nível, recebe 1 ponto de vida adicional</li>
+            </ul>
 
-    Elfo Da Floresta
-    <ul>
-        <li>O valor de Wisdom do personagem aumenta em 1.</li>
-        <li>O deslocamento do personagem aumenta para 35 feet.</li>
-    </ul>
+            Elfo Da Floresta
+            <ul>
+                <li>O valor de Wisdom do personagem aumenta em 1.</li>
+                <li>O deslocamento do personagem aumenta para 35 feet.</li>
+            </ul>
 
-    Elfo Negro
-    <ul>
-        <li>O valor de Charisma do personagem aumenta em 1.</li>
-        <li>Banidos da supreficie, pela sua devoção à deusa Lolth os personagem desta raça tem darkvision.</li>
-    </ul>
+            Elfo Negro
+            <ul>
+                <li>O valor de Charisma do personagem aumenta em 1.</li>
+                <li>Banidos da supreficie, pela sua devoção à deusa Lolth os personagem desta raça tem darkvision.</li>
+            </ul>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="3" class="sparador">
+            <img src="imagens/separator.png" style="width: 40%; height: 50%">
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2" class="texto">
+            <h3>Humano</h3>
+            Os humanos umas das raças comuns deste universo, com uma vida curta comparados a outras raças existentes. Por isso fazem o melhor com o pouco tempo que têm.
+            Por isso são uma raça inovadora e pioneira, tendo vástos impérios espalhados pelo mundo que duram eras e alguns nunca chegaram a ruir.
+            <p>
+                São uma raça que no geral mantem boas relações com as outras raças do mundo o que os ajuda a fazer poderosos aliados e a estabelcer comercio por todos os cantos do mundo.
+            </p>
+            <p>
+                Muitos humanos quando se tornam aventureiros fazem no por desejarem riquezas e fama ou para deixarem um legado grandioso, cuja história nunca será capaz de esquecer.
+            </p>
+            <h4>Traços da Raça:</h4>
+            <ul>
+                <li>São capazes de falar várias linguas por isso um personagem humano sabe Comum e outra lingua à sua escolha</li>
+                <li>São bastante equilibrados por isso um personagem ganha +1 em todos os atributos </li>
+            </ul>
+        </td>
+        <td class="imagens">
+            <img src="imagens/human2-removebg-preview.png">
+        </td>
+    </tr>
+    <tr>
+        <td colspan="3" class="sparador">
+            <img src="imagens/separator.png" style="width: 40%; height: 50%">
+        </td>
+    </tr>
+    <tr>
+        <td class="imagens">
+            <img src="imagens/dwarf-removebg-preview.png">
+        </td>
+
+        <td colspan="2" class="texto">
+                <h3>Anões</h3>
+                Reis e Lordes de reinos ricos de antiga grandeza, salões esculpidos no interiror das montanhas, o eco de picaretas nas minas mais profundas e martelos nas forjas ardentes conhecidas pelo mundo.
+                Orgulhosos da sua história e com um compromisso com o clã e a tradição os anões não quecem o passado lutam até ao ultimo suspiro para protegerem o que é seu.
+                <p>
+                    Os anões são leais e fiéis à sua palavra têm um forte senso de justiça e demoram a se esquecer quem os erra. No entantos se um anão estiver ao lado este nunca te irá abandonar e lutará por ti até ao fim.
+                    Trair um anão não é algo que seja sábio fazer pois o sentido de justiça destes, torna a vingança de um anão na vingança do seu clã.
+                </p>
+
+                <h4>Traços da Raça:</h4>
+                <ul>
+                    <li>Habituados a viver no interiror das montanhas o personagem desta raça tem darkvision.</li>
+                    <li>Fortes e robustos personagens desta raça ganha +2 na Constitution.</li>
+                </ul>
+                <h4>Sub-raças</h4>
+                Anão Da Colina
+                <ul>
+                    <li>O valor de Wisdom do personagem aumenta em 1.</li>
+                    <li>O máximo de pontos de vida do personagem aumenta em 1, e cada vez que sobe de nível, recebe 1 ponto de vida adicional</li>
+                </ul>
+                Anão da Montanha
+                <ul>
+                    <li>O valor de strengh do personagem aumenta em 2.</li>
+                    <li>O personagem tem proficiência em armaduras leves e médias.</li>
+                </ul>
+        </td>
+    </tr>
+</table>
 
 
-</div>
 
-<div>
-    <h3>Anões</h3>
-    Reis e Lordes de reinos ricos de antiga grandeza, salões esculpidos no interiror das montanhas, o eco de picaretas nas minas mais profundas e martelos nas forjas ardentes conhecidas pelo mundo.
-    Orgulhosos da sua história e com um compromisso com o clã e a tradição os anões não quecem o passado lutam até ao ultimo suspiro para protegerem o que é seu.
-    <p>
-        Os anões são leais e fiéis à sua palavra têm um forte senso de justiça e demoram a se esquecer quem os erra. No entantos se um anão estiver ao lado este nunca te irá abandonar e lutará por ti até ao fim.
-        Trair um anão não é algo que seja sábio fazer pois o sentido de justiça destes, torna a vingança de um anão na vingança do seu clã.
-    </p>
-
-    <h4>Traços da Raça:</h4>
-    <ul>
-        <li>Habituados a viver no interiror das montanhas o personagem desta raça tem darkvision.</li>
-        <li>Fortes e robustos personagens desta raça ganha +2 na Constitution.</li>
-    </ul>
-    <h4>Sub-raças</h4>
-    Anão Da Colina
-    <ul>
-        <li>O valor de Wisdom do personagem aumenta em 1.</li>
-        <li>O máximo de pontos de vida do personagem aumenta em 1, e cada vez que sobe de nível, recebe 1 ponto de vida adicional</li>
-    </ul>
-    Anão da Montanha
-    <ul>
-        <li>O valor de strengh do personagem aumenta em 2.</li>
-        <li>O personagem tem proficiência em armaduras leves e médias.</li>
-    </ul>
-
-</div>
 
 <div class="footer">DungeonPT.lmd no rights reserved</div>
 
--- classes.php
+++ classes.php
@@ -10,6 +10,19 @@ session_start();
     <link rel="stylesheet" href="CSS/raças.css">
 </head>
 
+<style>
+    table,
+    th,
+    td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        padding: 6px;
+    },
+    h4{
+        color: #581e00;
+    }
+</style>
+
 <body>
 
     <header>
@@ -86,102 +99,130 @@ session_start();
             </ul>
         </nav>
     </div>
-    <div>
-        <h3>Lutador</h3>
-        Um Lutador está sempre pronto para combate. Capáz de usar qualquer arma que deste espadas, machados e lanças a arcos para combate a
-        longa distância. Não lhe falta capacidades ofencivas mas também não lhe faltam defencivas, armadura e escudos são icónicos em este personagem.
-        Se queres ser um dos melhores guerreiros cujo nome é conecido em toda a terra por onde passas esta classe premite isso.
 
-        Para tal estas são as tuas habilidades iniciais:
-        <ul>
-            <li>
-                <h4>Second Wind:</h4>
-                No teu turno podes usar uma ação bónus para ganhar pontos de vida iguais a 1d10 + o teu nivel.
-                <br>Para poderes a voltar a usar esta habilidade tens de fazer um short rest ou um long rest.
-            </li>
-            <li>
-                <h4>Escolhes também o teu estilo de de combate da seguinte lista:</h4>
+    <table cellspacing="0" cellpadding="0" border="none">
+        <tr>
+            <td class="imagens">
+
+                <img src="imagens/warrior.png">
+
+            </td>
+            <td colspan="2" class="texto">
+                <h3>Lutador</h3>
+                Um Lutador está sempre pronto para combate. Capáz de usar qualquer arma que deste espadas, machados e lanças a arcos para combate a
+                longa distância. Não lhe falta capacidades ofencivas mas também não lhe faltam defencivas, armadura e escudos são icónicos em este personagem.
+                Se queres ser um dos melhores guerreiros cujo nome é conecido em toda a terra por onde passas esta classe premite isso.
+
+                Para tal estas são as tuas habilidades iniciais:
                 <ul>
                     <li>
-                        <h4>Archery:</h4> Ganhas +2 pontos de bónus em ataques com armas de longo alcance.
+                        <h4>Second Wind:</h4>
+                        No teu turno podes usar uma ação bónus para ganhar pontos de vida iguais a 1d10 + o teu nivel.
+                        <br>Para poderes a voltar a usar esta habilidade tens de fazer um short rest ou um long rest.
                     </li>
                     <li>
-                        <h4>Defense:</h4> Enquanto usas armadura ganha +1 na tua AC(Armor Class)
-                    </li>
-                    <li>
-                        <h4>Dueling:</h4> Ao usar uma arma de corpo a corpo numa só mão e sem outras armas ganha um bónus de +2 ao dano que dás.
-                    </li>
-                    <li>
-                        <h4>Great Weapon Fighting:</h4> Ae no dano te sair nos dados 1 ou 2 e se estiveres a usar uma arma corpo a corpo com as duas mãos,
-                        <br>podes voltar a laçar os dados, mas tens de usar este novo valor independentemente do resultado. A arma tem de ter as propriedades
-                        <br> de versátil ou uso em duas mãos.
-                    </li>
-                    <li>
-                        <h4>Protection:</h4> Ae uma criatura ataca outro alvo para além de ti e se está a 5ft de ti podes usar a tua reação para lhe causar
-                        <br>disvantagem no lançamento de ataque. Tens de estar a usar um escudo.
-                    </li>
-                    <li>
-                        <h4>Tow-Weapon Fighting:</h4> Quando estás a usar combate de duas armas adicionas o tue modificador de habilidade ao dano do teu segundo ataque.
+                        <h4>Escolhes também o teu estilo de de combate da seguinte lista:</h4>
+                        <ul>
+                            <li>
+                                <b>Archery:</b> Ganhas +2 pontos de bónus em ataques com armas de longo alcance.<br><br>
+                            </li>
+                            <li>
+                                <b>Defense:</b> Enquanto usas armadura ganha +1 na tua AC(Armor Class)<br><br>
+                            </li>
+                            <li>
+                                <b>Dueling:</b> Ao usar uma arma de corpo a corpo numa só mão e sem outras armas ganha um bónus de +2 ao dano que dás.<br><br>
+                            </li>
+                            <li>
+                                <b>Great Weapon Fighting:</b> Ao no dano te sair nos dados 1 ou 2 e se estiveres a usar uma arma corpo a corpo com as duas mãos,
+                                podes voltar a laçar os dados, mas tens de usar este novo valor independentemente do resultado. A arma tem de ter as propriedades
+                                de versátil ou uso em duas mãos. <br><br>
+                            </li>
+                            <li>
+                                <b>Protection:</b> Ae uma criatura ataca outro alvo para além de ti e se está a 5ft de ti podes usar a tua reação para lhe causar
+                                disvantagem no lançamento de ataque. Tens de estar a usar um escudo.<br><br>
+                            </li>
+                            <li>
+                                <b>Tow-Weapon Fighting:</b> Quando estás a usar combate de duas armas adicionas o tue modificador de habilidade ao dano do teu segundo ataque.<br><br>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
-            </li>
-        </ul>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3" class="sparador">
+                <img src="imagens/separator.png" style="width: 40%; height: 50%">
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" class="texto">
+                <h3>Rouge</h3>
+                Mestres da fortividade, engano, espionagem, o rogue tem uma destresa enorme que lhe premite brilhar é muitas situações, desde batedores a assassinos.
+                Estes personagens são capazes de encontrar uma solução a quase todos os problemas. Ninguem sabe bem a origem destes individuos, mas podes ter a certeza
+                que te vais crusar com uns quantos deles nas tuas aventuras.
+                <br>Se andar pelas sombras sem ninguém saber, espiar os teus alvos e descubrir o método mais rápido para obteres o que queres então rogue é a tua classe.
 
-    </div>
+                Para tal estas são as tuas habilidades iniciais:
 
-    <div>
-        <h3>Rouge</h3>
-        Mestres da fortividade, engano, espionagem, o rogue tem uma destresa enorme que lhe premite brilhar é muitas situações, desde batedores a assassinos.
-        Estes personagens são capazes de encontrar uma solução a quase todos os problemas. Ninguem sabe bem a origem destes individuos, mas podes ter a certeza
-        que te vais crusar com uns quantos deles nas tuas aventuras.
-        <br>Se andar pelas sombras sem ninguém saber, espiar os teus alvos e descubrir o método mais rápido para obteres o que queres então rogue é a tua classe.
+                <ul>
+                    <li>
+                        <h4>Expertise:</h4>
+                        Escolhe duas skill proficiencies ou uma em conjunto com proficiencie em ferramentas de ladão.
+                        <br>O teu bónus de habilidade é duplicado quando te é pedido um check dessa habilidade.
+                    </li>
+                    <li>
+                        <h4>Sneak Attack:</h4>
+                        Uma vez por turno podes causar mais 1d6 de dano se acertares numa criatura sobre a qual tens vantagem
+                        <br>no lançamento de ataque ou se um outro inimigo dessa criatura está a 5ft dela, se não tiveres desvantagem no lançamento de ataque.
+                        <br>Tens de estar a usar uma arma com finesse ou uma arma de longo alcance.
+                    </li>
+                    <li>
+                        <h4>Thieves Cant:</h4>
+                        Conheces esta liguagem secreta dos rogues, podes comunicar e entender esta linguagem e preceber simbulos e pequenas mensagens escritas nesta linguagem.
+                        <br>Para quem não conhece o que está a ser dito ou escrito não faz sentido.
+                    </li>
+                </ul>
+            </td>
+            <td class="imagens">
+                <img src="imagens/rouge.png">
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3" class="sparador">
+                <img src="imagens/separator.png" style="width: 40%; height: 50%">
+            </td>
+        </tr>
+        <tr>
+            <td class="imagens">
+                <img src="imagens/wizard-removebg-preview.png">
+            </td>
 
-        Para tal estas são as tuas habilidades iniciais:
+            <td colspan="2" class="texto">
+                <h3>Feiticeiro</h3>
+                Poder flui nas mãos destes estudiosos que procuram desvendar todos os mistérios da magia. dedicados ao estudo das artes arcanas, os feiticeiros têm acesso a uma
+                variedades de feitiços que lhes abordar qualquer situção. Mas lá por serem estudiosos esta classe tem muito poder de fogo, capazes de dominar os elementos, criar
+                ilusões poderosas, e ajudar os seus companheiros em combate ou com o seu vasto conhecimento.
+                <br>Se dominar a magia, lançar feitços poderosos e ter aventuras para cresceres os teus poderes e o teu conhecimento do arcano, então esta é a tua classe
 
-        <ul>
-            <li>
-                <h4>Expertise:</h4>
-                Escolhe duas skill proficiencies ou uma em conjunto com proficiencie em ferramentas de ladão.
-                <br>O teu bónus de habilidade é duplicado quando te é pedido um check dessa habilidade.
-            </li>
-            <li>
-                <h4>Sneak Attack:</h4>
-                Uma vez por turno podes causar mais 1d6 de dano se acertares numa criatura sobre a qual tens vantagem
-                <br>no lançamento de ataque ou se um outro inimigo dessa criatura está a 5ft dela, se não tiveres desvantagem no lançamento de ataque.
-                <br>Tens de estar a usar uma arma com finesse ou uma arma de longo alcance.
-            </li>
-            <li>
-                <h4>Thieves Cant:</h4>
-                Conheces esta liguagem secreta dos rogues, podes comunicar e entender esta linguagem e preceber simbulos e pequenas mensagens escritas nesta linguagem.
-                <br>Para quem não conhece o que está a ser dito ou escrito não faz sentido.
-            </li>
-        </ul>
-    </div>
+                Para isso estas são as tuas habilidades iniciais:
+                <ul>
+                    <li>
+                        <h4>Spellcasting:</h4>
+                        Como um utilizador de magia e das artes arcanas , tens um livro de feitiços que te premite usar os feitiços da lista da tua classe.
+                        Para começar o teu livro contem 6 feitiços de nivel 1, este livro é o teu repositório de feitiços que à medida que evoluis vai conter mais e mais feitiços,
+                        conheces também 3 catrips.
 
+                    </li>
+                    <li>
+                        <h4>Arcane Recovery:</h4>
+                        Quando acabas um descanso curto podes recuperear alguns dos teus slots de feitiços, a soma do nivel dos slots recuperados não pode ser igual ou maior que
+                        o teu nivel, também não podes recuperar os teus slots de nivel 6 ou superior.
+                    </li>
+                </ul>
+            </td>
+        </tr>
+    </table>
 
-    <div>
-        <h3>Feiticeiro</h3>
-        Poder flui nas mãos destes estudiosos que procuram desvendar todos os mistérios da magia. dedicados ao estudo das artes arcanas, os feiticeiros têm acesso a uma
-        variedades de feitiços que lhes abordar qualquer situção. Mas lá por serem estudiosos esta classe tem muito poder de fogo, capazes de dominar os elementos, criar
-        ilusões poderosas, e ajudar os seus companheiros em combate ou com o seu vasto conhecimento.
-        <br>Se dominar a magia, lançar feitços poderosos e ter aventuras para cresceres os teus poderes e o teu conhecimento do arcano, então esta é a tua classe
-
-        Para isso estas são as tuas habilidades iniciais:
-        <ul>
-            <li>
-                <h4>Spellcasting:</h4>
-                Como um utilizador de magia e das artes arcanas , tens um livro de feitiços que te premite usar os feitiços da lista da tua classe.
-                Para começar o teu livro contem 6 feitiços de nivel 1, este livro é o teu repositório de feitiços que à medida que evoluis vai conter mais e mais feitiços,
-                conheces também 3 catrips.
-
-            </li>
-            <li>
-                <h4>Arcane Recovery:</h4>
-                Quando acabas um descanso curto podes recuperear alguns dos teus slots de feitiços, a soma do nivel dos slots recuperados não pode ser igual ou maior que
-                o teu nivel, também não podes recuperar os teus slots de nivel 6 ou superior.
-            </li>
-        </ul>
-    </div>
 
     <div class="footer">DungeonPT.lmd no rights reserved</div>
 
